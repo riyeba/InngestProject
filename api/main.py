486677f67a876348fd5bc05e14f6451e912096ee
Taiwo <moriyebas@gmail.com>
2026-01-13 00:36:23 +0300

    21st commit

--- api/main.py
+++ api/main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, File, UploadFile, Form,Request
 import requests
 from vercel import blob
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # LlamaIndex Imports
@@ -107,7 +108,19 @@ async def import_product_documents(ctx: inngest.Context):
 # 5. FastAPI Setup
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",  # Your local frontend
+    "http://127.0.0.1:5173",
+    "https://inngest-project.vercel.app", # Your backend itself (for production safety)
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
